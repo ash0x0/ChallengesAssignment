e82fc3174c147348d56069160758ed42aa1ca1fe
Ahmed Elshafey <ahmed.e.elshafey@gmail.com>
2018-02-27 13:18:51 +0000

    iscos house transferred to file input

--- L.isco.house.cpp
+++ L.isco.house.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <string>
 #include <fstream>
- #include <cstring>
+
 using namespace std;
-static const string filename = "self.in";
+static const string filename = "isco.in";
 ifstream file(filename);
 string currentString;
 
 int main () {
-     int  x,y,z,p;
-    
+    int  x,y,z,p;
     // get the test case number and disregard it
-    getline(cin, currentString);
+    getline(file, currentString);
     while (true) {
-    getline(cin, currentString, ' ');
-    x=atoi(currentString.c_str());
-    getline(cin, currentString, ' ');
-    y=atoi(currentString.c_str());
-    getline(cin, currentString, ' ');
-    p=atoi(currentString.c_str());
-    getline(cin, currentString);
-    z=atoi(currentString.c_str());
-        
-    cout<<((x*y)-z)*p<<endl;
-if(cin.eof()) break;
-    	
+        getline(file, currentString, ' ');
+        x=atoi(currentString.c_str());
+        getline(file, currentString, ' ');
+        y=atoi(currentString.c_str());
+        getline(file, currentString, ' ');
+        p=atoi(currentString.c_str());
+        getline(file, currentString);
+        z=atoi(currentString.c_str());
+        cout<<((x*y)-z)*p<<endl;
+        if(cin.eof()) break;
     }
-    
-    return 0;}
+    return 0;
+}
     
  
