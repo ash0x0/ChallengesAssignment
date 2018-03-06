55362b64354742db83ad344097573b040f7b903b
Ahmed Elshafey <ahmed.e.elshafey@gmail.com>
2018-03-06 18:58:52 +0000

    isco house

--- L.isco.house.cpp
+++ L.isco.house.cpp
@@ -9,19 +9,28 @@ string currentString;
 
 int main () {
     int  x,y,z,p;
-    // get the test case number and disregard it
     getline(file, currentString);
     while (true) {
-        getline(file, currentString, ' ');
-        x=atoi(currentString.c_str());
-        getline(file, currentString, ' ');
-        y=atoi(currentString.c_str());
-        getline(file, currentString, ' ');
-        p=atoi(currentString.c_str());
         getline(file, currentString);
-        z=atoi(currentString.c_str());
-        cout<<((x*y)-z)*p<<endl;
-        if(cin.eof()) break;
+        std::istringstream iss(text);
+        std::vector<std::string> results((std::istream_iterator<WordDelimitedBy<' '>>(iss)), std::istream_iterator<WordDelimitedBy<' '>>());
+        cout << results << endl;
+        // n = stoi(currentString.substr(0, currentString.find(" ")));
+        // k = stoi(currentString.substr(currentString.find(" "), currentString.length() -1));
+        // for (char c : currentString) {
+        //     if (c == " ") {
+                
+        //     }
+        // }
+        // x=atoi(currentString.c_str());
+        // getline(file, currentString, ' ');
+        // y=atoi(currentString.c_str());
+        // getline(file, currentString, ' ');
+        // p=atoi(currentString.c_str());
+        // getline(file, currentString);
+        // z=atoi(currentString.c_str());
+        // cout<<((x*y)-z)*p<<endl;
+        // if(cin.eof()) break;
     }
     return 0;
-}qqqqq
+}
