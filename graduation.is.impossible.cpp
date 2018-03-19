d088145b12c9f6edbd1f6597b8252c08447d5444
Ahmed Elshafey <ahmed.e.elshafey@gmail.com>
2018-03-19 19:32:41 +0000

    graduation is impossible

--- graduation.is.impossible.cpp
+++ graduation.is.impossible.cpp
@@ -1,22 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-ios_base::sync_with_stdio(false);
 
 int main() {
-
+    ios_base::sync_with_stdio(false);
     freopen("graduation.in" , "r" , stdin);
     int t, num;
     int x, y;
-    int x1, y1;
     cin >> t;
     while(t--){
         x = 0; 
         y = 0;
-        x1 = 0; 
-        y1 = 0;
         num = 0;
         cin >> x >> y;
+        (y > x) ? num = ceil(y / 6.0) : num = ceil(x / 6.0);
+        num *= 2;
+        if (x > y || y < x) num--;
+        // num = ceil(x/6.0) + ceil(y/6.0);
+        // if (x / 6 > y) {
+            
+        // }
+        // if (y / 6 > x) {
+            
+        // }
+        // if (y > x) num += y / 6;
+        // if (x > y) num += x / 6;
         
+        
+        cout << num << endl;
     }
+    
     return 0;
 }
